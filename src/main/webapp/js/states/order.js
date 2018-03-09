a927ea80d9f1f93a8d9cae75e90bb1c5f380ff81
kunalgadhia@gmail.com <kunalgadhia@gmail.com>
2018-03-09 13:40:13 +0530

    Order Commercials Added

--- src/main/webapp/js/states/order.js
+++ src/main/webapp/js/states/order.js
@@ -73,15 +73,28 @@ angular.module("sales.states.order", [])
 
         })
         .controller('OrderDetailsController', function ($state, OrderDetailsService, SkuStockService, OrderHeadService, $stateParams, $rootScope, $scope, UserService) {
+            $scope.editableOrderHead = {};
             OrderHeadService.get({
                 'id': $stateParams.orderHeadId
             }, function (orderHeadObject) {
                 $scope.orderHeadObject = orderHeadObject;
+                $scope.editableOrderHead = orderHeadObject;
+                $scope.editableOrderHead.transportationCharges = 750;
             });
 
             $scope.orderDetailsList = OrderDetailsService.findByOrderHeadId({
                 'orderHeadId': $stateParams.orderHeadId
             });
+
+//            $scope.$watch('orderDetailsList', function (orderDetaisList) {
+//                console.log("orderDetails List :%O", $scope.orderDetailsList);
+//                var a = 0;
+//                angular.forEach(orderDetaisList, function (orderObject) {
+//                    a = a + orderObject.price;
+//                    console.log("$scope.totalPrice :%O", a);
+//                    $scope.editableOrderHead.totalAmount = a;
+//                });
+//            });
 
             $scope.searchSku = function (skuString) {
                 return SkuStockService.findByNameLike({
@@ -98,33 +111,47 @@ angular.module("sales.states.order", [])
             $scope.$watch('editableOrderDetail.quantity', function (productQuantity) {
                 $scope.editableOrderDetail.price = ($scope.skuObject.price * productQuantity);
             });
+            $scope.$watch('editableOrderHead.installationCharges', function (installationCharges) {
+                console.log("Transporation :" + $scope.editableOrderHead.transportationCharges);
+                console.log("Installation :" + installationCharges);
+                $scope.orderDetailsArray = OrderDetailsService.findByOrderHeadId({
+                    'orderHeadId': $stateParams.orderHeadId
+                }, function (orderDetailsList) {
+                    var b = 0;
+                    angular.forEach(orderDetailsList, function (orderDetail) {
+                        b = b + orderDetail.price;
+                        $scope.totalOrderPrice = b;
+                    });
+                });
+                console.log("Total Order price :%O", $scope.totalOrderPrice);
+                $scope.editableOrderHead.totalAmount = ($scope.editableOrderHead.transportationCharges + installationCharges + $scope.totalOrderPrice);
+            });
             $scope.stockOver = false;
             $scope.disableDetailSave = false;
             $scope.saveOrderDetail = function (editableOrderDetail) {
                 $scope.t48Stock = $scope.skuObject.t48Stock;
                 $scope.nimjiStock = $scope.skuObject.nimjiStock;
-                if ($scope.t48Stock >= editableOrderDetail.quantity) {                    
+                if ($scope.t48Stock >= editableOrderDetail.quantity) {
                     $scope.skuObject.t48Stock = ($scope.skuObject.t48Stock - editableOrderDetail.quantity);
                     $scope.skuObject.totalStock = ($scope.skuObject.totalStock - editableOrderDetail.quantity);
-                    editableOrderDetail.productLocation = "T48";                    
+                    editableOrderDetail.productLocation = "T48";
                     $scope.skuObject.$save();
-                } else if ($scope.t48Stock <= editableOrderDetail.quantity) {                    
-                    if ($scope.nimjiStock >= editableOrderDetail.quantity) {                        
+                } else if ($scope.t48Stock <= editableOrderDetail.quantity) {
+                    if ($scope.nimjiStock >= editableOrderDetail.quantity) {
                         $scope.skuObject.nimjiStock = ($scope.skuObject.nimjiStock - editableOrderDetail.quantity);
                         $scope.skuObject.totalStock = ($scope.skuObject.totalStock - editableOrderDetail.quantity);
-                        editableOrderDetail.productLocation = "NIMJI";                        
+                        editableOrderDetail.productLocation = "NIMJI";
                         $scope.skuObject.$save();
                     } else {
                         $scope.stockOver = true;
-                        $scope.disableDetailSave = true;                        
+                        $scope.disableDetailSave = true;
                     }
                 }
-
                 editableOrderDetail.productId = $scope.skuObject.id;
                 editableOrderDetail.productCode = $scope.skuObject.sku;
                 editableOrderDetail.productName = $scope.skuObject.productDescription;
                 editableOrderDetail.productColor = $scope.skuObject.color;
-                editableOrderDetail.orderHeadId = $stateParams.orderHeadId;                
+                editableOrderDetail.orderHeadId = $stateParams.orderHeadId;
                 OrderDetailsService.save(editableOrderDetail, function (orderD) {
                     $state.go('admin.order_details', {
                         'orderHeadId': $stateParams.orderHeadId
@@ -133,10 +160,10 @@ angular.module("sales.states.order", [])
             };
 
         })
-        .controller('OrderDetailDeleteController', function (SkuStockService, OrderDetailsService, $scope, $stateParams, $state, paginationLimit) {            
+        .controller('OrderDetailDeleteController', function (SkuStockService, OrderDetailsService, $scope, $stateParams, $state, paginationLimit) {
             $scope.editableOrderDetail = OrderDetailsService.get({'id': $stateParams.orderDetailId});
-            $scope.deleteOrderDetail = function (orderDetail) {                
-                if (orderDetail.productLocation === "T48") {                    
+            $scope.deleteOrderDetail = function (orderDetail) {
+                if (orderDetail.productLocation === "T48") {
                     $scope.skuObject = SkuStockService.get({
                         'id': orderDetail.productId
                     }, function (skuObject) {
